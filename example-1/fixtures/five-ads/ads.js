92fa6243dde02141194ad4c9d5ebcf1d52e0fb98
pontakornth <pontakorn61@gmail.com>
2023-02-25 14:37:43 +0700

    match fixture

--- example-1/fixtures/five-ads/ads.js
+++ example-1/fixtures/five-ads/ads.js
@@ -3,22 +3,27 @@ const mongodb = require("mongodb");
 module.exports = [
   {
     _id: mongodb.ObjectId("5ea244a1c34230004592eb32"),
-    name: "Gunshin Impact",
-    link: "https://google.com",
+    name: "Genshin Impact",
+    link: "https://genshin.hoyoverse.com/en/",
   },
   {
     _id: mongodb.ObjectId("5ea244a5c34230004592eb33"),
-    name: "Baka Mitai Startup",
-    link: "https://dev.to",
+    name: "Agoda",
+    link: "https://agoda.com",
   },
   {
     _id: mongodb.ObjectId("5ea244a1c34230004592eb34"),
-    name: "Senior Webmaster Camp 1",
-    link: "https://pontakorn.dev",
+    name: "Lazada",
+    link: "https://lazada.co.th",
   },
   {
     _id: mongodb.ObjectId("5ea244a5c34230004592eb35"),
-    name: "Mindcraft",
-    link: "https://minecraft.net",
+    name: "Chess.com",
+    link: "https://chess.com",
+  },
+  {
+    _id: mongodb.ObjectId("5ea244a5c34230004592eb36"),
+    name: "Masuri",
+    link: "https://shopee.co.th/Mazuri-5M6c-%E0%B8%AD%E0%B8%B2%E0%B8%AB%E0%B8%B2%E0%B8%A3%E0%B8%AA%E0%B8%B1%E0%B8%95%E0%B8%A7%E0%B9%8C%E0%B8%81%E0%B8%B4%E0%B8%99%E0%B9%81%E0%B8%A1%E0%B8%A5%E0%B8%87-%E0%B8%81%E0%B8%A3%E0%B8%B0%E0%B8%AA%E0%B8%AD%E0%B8%9A11.33%E0%B8%81%E0%B8%B4%E0%B9%82%E0%B8%A5%E0%B8%81%E0%B8%A3%E0%B8%B1%E0%B8%A1-i.63446565.1944721192",
   },
 ];
